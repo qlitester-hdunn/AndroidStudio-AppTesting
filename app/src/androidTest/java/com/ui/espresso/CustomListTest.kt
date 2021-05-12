703b227e4d1ee6297520e06185bb7bf22b4fe2ed
hdunn <qlihodunn@gmail.com>
2021-05-12 00:30:02 -0600

    test a

--- app/src/androidTest/java/com/ui/espresso/CustomListTest.kt
+++ app/src/androidTest/java/com/ui/espresso/CustomListTest.kt
@@ -1,20 +1,33 @@
 package com.ui.espresso
 
+import android.util.Log
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
-import com.ui.espresso.bases.CustomListActivityBase
+import com.ui.espresso.bases.TestBase
 import com.ui.espresso.matchers.CustomMatchers
 import org.hamcrest.CoreMatchers
+import org.junit.Before
 import org.junit.Test
 
 /**
- * @author  HDunn, Modified on 4/30/21.
+ * @author  HDunn, Modified on 5/12/21.
+ *
+ * TEST - MAKE ALL PASS
  */
-class CustomListTest : CustomListActivityBase() {
+class CustomListTest : TestBase() {
+
+    @Before
+    fun navigateToBooksList(){
+        Log.i("Navigate","runs now")
+    }
+
     @Test
     fun testOpenBookById() {
+        Log.i("test","starting test open book")
+
+        Espresso.onView(ViewMatchers.withId(R.id.custom_list_adapter_button)).perform(ViewActions.click())
 
         // Click on the Book with ID 5
         Espresso.onData(CustomMatchers.withBookId(5)).perform(ViewActions.click())
@@ -28,6 +41,7 @@ class CustomListTest : CustomListActivityBase() {
 
     @Test
     fun testOpenBookByTitleAndAuthor() {
+
         // Match a book with a specific title and author name
         Espresso.onData(CoreMatchers.allOf(CustomMatchers.withBookTitle(BOOK_TITLE), CustomMatchers.withBookAuthor(BOOK_AUTHOR))).perform(ViewActions.click())
 
