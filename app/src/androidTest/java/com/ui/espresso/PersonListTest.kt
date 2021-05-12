bfdea5e54be902c10f3a70eb21d3048986e6de94
hdunn <qlihodunn@gmail.com>
2021-05-12 08:44:41 -0600

    test-c

---
+++ app/src/androidTest/java/com/ui/espresso/PersonListTest.kt
@@ -0,0 +1,14 @@
+package com.ui.espresso
+
+import com.ui.espresso.bases.TestBase
+
+/**
+ *   WORK HERE AND CODE AS MUCH TESTING AS YOU WANT
+ *   - verify like "Code Master" is displayed
+ *   - verify like flaticon is in view.
+ *   - verify age or message for "tester"
+ */
+class PersonListTest : TestBase() {
+    // WORK HERE test-c
+
+}
